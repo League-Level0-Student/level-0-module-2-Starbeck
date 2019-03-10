5e65e3b49f057feda7aee9d08e0952ac9de3ad19
sepfish <40220408+sepfish@users.noreply.github.com>
2019-03-09 17:30:09 -0800

    Started Level 0 Module 2

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -5,10 +5,12 @@ package random;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class IntroToRandom {
 	public static void main(String[] args) {
 		
-		int r = 0;
+		Random r = new Random();
 		
 		//3. Now make r random. 
 		//   Create an object of the Random class
@@ -16,13 +18,15 @@ public class IntroToRandom {
 		
 
 		//4. Limit the random number between 0 and 100
-
+int t = r.nextInt(101);
 		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
-
+int n = r.nextInt((75-25)+1)+25;
 		//6. Challenge: Limit the random number between -222 and 88
-
+int l = r.nextInt((310)+1)-222;
 		//1. Print out the value of r
-		
+		JOptionPane.showMessageDialog(null,n);
+		JOptionPane.showMessageDialog(null,t);
+		JOptionPane.showMessageDialog(null,l);
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?
 	}
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -17,7 +17,19 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+for(int i=0; i<=10; i++) {
+		if (randomNumber == 0) {
+			
+		} else if (randomNumber == 1) {
+			
+		} else if (randomNumber == 2) {
+			
+		} else if (randomNumber == 3) {
+			
+		} else {
+			
+		}
+	};
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
